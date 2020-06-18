cb8ec8215acd65844e1b0820c4782249dfade1fb
DanilChandra <55772104+DanilChandra@users.noreply.github.com>
2020-06-18 20:39:50 +0700

    menghapus baris alamat menambahkan no hp dan email

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo 'Nama : Daniel Chandra <br>';
 echo 'Kelas : XI RPL 1 <br>';
 echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
-echo 'Alamat Rumah : Jl.Pekapuran Gg.Ripan No.38 RT 07 / RW 05 Tapos Depok <br>';
+echo 'Nomor Hp : 08975951040 <br>';
+echo 'Email : danilchandra1809@gmail.com <br>';
 ?>
