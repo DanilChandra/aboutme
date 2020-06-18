075badf9fb676859a7f5a2749f3ce4222f69fa4b
DanilChandra <55772104+DanilChandra@users.noreply.github.com>
2020-06-18 20:37:33 +0700

    Create aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+echo 'Nama : Daniel Chandra <br>';
+echo 'Kelas : XI RPL 1 <br>';
+echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
+echo 'Alamat Rumah : Jl.Pekapuran Gg.Ripan No.38 RT 07 / RW 05 Tapos Depok <br>';
+?>
